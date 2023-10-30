f2d1a5d302ea39741f025764f627dae867986e12
J jet <ovepje2004@gmail.com>
2023-10-30 18:03:07 +0900

    Update 11_example_1.ino

--- 11_example_1.ino
+++ 11_example_1.ino
@@ -11,7 +11,7 @@
 #define INTERVAL 25      // sampling interval (unit: msec)
 #define PULSE_DURATION 10 // ultra-sound Pulse Duration (unit: usec)
 #define _DIST_MIN 100.0   // minimum distance to be measured (unit: mm)
-#define _DIST_MAX 400.0   // maximum distance to be measured (unit: mm)
+#define _DIST_MAX 300.0   // maximum distance to be measured (unit: mm)
 
 #define TIMEOUT ((INTERVAL / 2) * 1000.0) // maximum echo waiting time (unit: usec)
 #define SCALE (0.001 * 0.5 * SND_VEL) // coefficent to convert duration to distance
